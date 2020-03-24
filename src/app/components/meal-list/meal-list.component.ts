8baac4f775f16d5f31378e5067a13692b3f757cb
Brady Liechty <berrybloxinator@gmail.com>
2020-03-24 10:49:58 -0600

    Can now search by category

--- src/app/components/meal-list/meal-list.component.ts
+++ src/app/components/meal-list/meal-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MealsService } from 'src/app/services/meals.service';
 import { combineLatest } from 'rxjs';
-import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-meal-list',
@@ -12,22 +12,36 @@ import { Router } from '@angular/router';
 export class MealListComponent implements OnInit {
   desserts: any[];
   americanMeals: any[];
+  categories: any[];
+  selectedMeals: any[];
+  selectedCategory;
 
   constructor(private mealsService: MealsService, private router: Router) { }
 
   ngOnInit(): void {
     combineLatest([
       this.mealsService.getMealsByFirstLetter('b'),
-      this.mealsService.getMealsByFirstLetter('c')
+      this.mealsService.getMealsByFirstLetter('c'),
+      this.mealsService.getAllCategories()
     ]).subscribe(data => {
       const meals = data[0].concat(data[1]);
       
       this.desserts = meals.filter(meal => meal.strCategory === 'Dessert');
       this.americanMeals = meals.filter(meal => meal.strArea === 'American');
+      this.categories = data[2];
     });
   }
 
   goToMealDetails(id: string) :void {
     this.router.navigate(['/meal-details', {id}]);
+  }
+
+  search(): void {
+    this.mealsService.getMealsByCategory(this.selectedCategory).pipe(
+      tap(data => {
+        this.selectedMeals = data.meals;
+        console.log(this.selectedMeals);
+      })
+    ).subscribe();
   }
 }
--- src/app/services/meals.service.ts
+++ src/app/services/meals.service.ts
@@ -9,6 +9,8 @@ import { map } from 'rxjs/operators';
 export class MealsService {
   mealsByFirstLetter: string = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
   mealDetails: string = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+  categoryUrl: string = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=';
+  categories: any[];
 
   constructor(private http: HttpClient) { }
 
@@ -22,5 +24,15 @@ export class MealsService {
     return this.http.get(this.mealDetails + id).pipe(
       map(meal => meal['meals'][0])
     );
+  }
+
+  getAllCategories(): Observable<any> {
+    return this.http.get('https://www.themealdb.com/api/json/v1/1/categories.php').pipe(
+      map((categories: {categories: any[]}) => categories.categories)
+    );
+  }
+
+  getMealsByCategory(category): Observable<any> {
+    return this.http.get(this.categoryUrl + category);
   }
 }
